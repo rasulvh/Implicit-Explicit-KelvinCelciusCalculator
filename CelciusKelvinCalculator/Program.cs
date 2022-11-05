feca4435c3fadbf15ee35cfc96a01f71535fe12a
rasulvh <rasulvh@code.edu.az>
2022-11-05 16:19:48 +0400

    kelvin celcius calculator was added

---
+++ CelciusKelvinCalculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CelciusKelvinCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Celcius celcius = new Celcius(36);
+
+            Kelvin kelvin = celcius;
+
+            Console.WriteLine(kelvin.KelvinDegree);
+        }
+    }
+
+    public class Kelvin
+    {
+        public double KelvinDegree { get; set; }
+
+        public Kelvin(double kelvinDegree)
+        {
+            KelvinDegree = kelvinDegree;
+        }
+    }
+
+    public class Celcius
+    {
+        public double CelciusDegree { get; set; }
+
+        public Celcius(double celciusDegree)
+        {
+            CelciusDegree = celciusDegree;
+        }
+
+        public static implicit operator Kelvin(Celcius celcius)
+        {
+            Kelvin kelvin = new Kelvin(celcius.CelciusDegree + 273);
+            return kelvin;
+        }
+    }
+}
